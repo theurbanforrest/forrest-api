4e27ae6dedf67a74b7a0a9187aa278e84bae106f
Forrest <forrest.ching@gmail.com>
2018-06-03 12:10:45 -0400

    trying out loopback-ssl via npm

--- server/server.js
+++ server/server.js
@@ -1,9 +1,79 @@
+/*** server.js from loopback-ssl library ***/
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var loopbackSSL = require('loopback-ssl');
+ 
+var app = module.exports = loopback();
+ 
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+});
+ 
+return loopbackSSL.startServer(app);
+
+
+
+
+
+
+
+/*** ======================= ***/
+
+
+
+
+
+
+
+/*** server.js that works on http
+
+  'use strict';
+
+  var loopback = require('loopback');
+  var boot = require('loopback-boot');
+
+  var app = module.exports = loopback();
+
+  app.start = function() {
+    // start the web server
+    return app.listen(function() {
+      app.emit('started');
+      var baseUrl = app.get('url').replace(/\/$/, '');
+      console.log('Web server listening at: %s', baseUrl);
+      if (app.get('loopback-component-explorer')) {
+        var explorerPath = app.get('loopback-component-explorer').mountPath;
+        console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      }
+    });
+  };
+
+  // Bootstrap the application, configure models, datasources and middleware.
+  // Sub-apps like REST API are mounted via boot scripts.
+  boot(app, __dirname, function(err) {
+    if (err) throw err;
+
+    // start the server if `$ node server.js`
+    if (require.main === module)
+      app.start();
+  });
+***/
+
+
+
+
+
+/*** ======================= ***/
+
+
+
+
+
 /*** server.js attempt using https with Loopback's official docs
 // Copyright IBM Corp. 2014,2016. All Rights Reserved.
 // Node module: loopback-example-ssl
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-***/
+
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
@@ -48,37 +118,4 @@ if (require.main === module) {
   app.start();
 }
 
-
-
-/*** server.js that works on http
-
-  'use strict';
-
-  var loopback = require('loopback');
-  var boot = require('loopback-boot');
-
-  var app = module.exports = loopback();
-
-  app.start = function() {
-    // start the web server
-    return app.listen(function() {
-      app.emit('started');
-      var baseUrl = app.get('url').replace(/\/$/, '');
-      console.log('Web server listening at: %s', baseUrl);
-      if (app.get('loopback-component-explorer')) {
-        var explorerPath = app.get('loopback-component-explorer').mountPath;
-        console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-      }
-    });
-  };
-
-  // Bootstrap the application, configure models, datasources and middleware.
-  // Sub-apps like REST API are mounted via boot scripts.
-  boot(app, __dirname, function(err) {
-    if (err) throw err;
-
-    // start the server if `$ node server.js`
-    if (require.main === module)
-      app.start();
-  });
 ***/
