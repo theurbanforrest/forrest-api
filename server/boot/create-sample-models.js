6a9b332ecee1c9534520706db0df52fe2036452c
Forrest <forrest.ching@gmail.com>
2018-12-08 13:56:29 -0500

    disabled boot

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 
   /*** BOOT DATA FOR FORREST'S NEW EVENT MANAGER
   ***
-  ***/
+  ***
 
   /// GROUP
 
@@ -93,7 +93,7 @@ module.exports = function(app) {
 
   /**/
   /// ENTRY
-  /**/
+  /**
     app.dataSources.undercrowd_p1.automigrate('Entry', function(err){
       
       app.models.Entry.create([{
